007b289d0a1f9f545c89d5dc3a91608b83480d13
James Sleeman <james@gogo.co.nz>
2016-06-05 22:40:36 +1200

    Further tidy up and documentation for examples.
    Note documentation added about pinMode() importantly, for the tiny13 pinMode(A...) is not going to do what the user thinks it should.  pinMode() should only be used with digital pin numbers!

--- avr/libraries/ATTinyCore/examples/Tiny13/01.Basics/BareMinimum/BareMinimum.ino
+++ avr/libraries/ATTinyCore/examples/Tiny13/01.Basics/BareMinimum/BareMinimum.ino
@@ -27,14 +27,33 @@
   4.8MHz will be 9600 Baud
   1.2MHz will be 9600 Baud
   
+  If you get garbage output:
+  
+    1. Check baud rate as above
+    2. Check if you have anything else connected to TX/RX like an LED
+    3. Check OSCCAL (see Examples > 05.Tools > OSCCAL_Helper    
+  
   Pinout
   ------------------------------------------------------------------------------
   For ATTiny13 Arduino Pinout: https://goo.gl/ijL0of  
-
-  You don't have much flash or ram to work with.  Remember to think about data type sizes!
+  
+  Important: 
+    pinMode() must only be used with the "digital pin numbers" 0 .. n
+    pins default to INPUT, you do not need to pinMode() to INPUT if you are only
+    ever doing an analogRead() from the pin. 
+    
+    analogRead() must only be used with the "analog pin numbers" A0 .. n
+        
+  
+  Space Saving Tips
+  ------------------------------------------------------------------------------
+  
+  You don't have much flash or ram to work with.  Remember to think about 
+  datatype sizes!  Use the options under the Tools menu to reduce capabilities
+  hopefully in exchange for more code size (especially Millis and Print).
 
   Running short on memory, try this tool to help you track down optimisable areas:
-    http://sparks.gogo.co.nz/avr-ram-use.html
+    http://sparks.gogo.co.nz/avr-ram-use.html  
   
   Good Luck With Your Itsy Bitsy Teeny Weeny AVR Arduineee
   
--- avr/libraries/ATTinyCore/examples/Tiny13/03.Analog/AnalogInOutPWM/AnalogInOutPWM.ino
+++ avr/libraries/ATTinyCore/examples/Tiny13/03.Analog/AnalogInOutPWM/AnalogInOutPWM.ino
@@ -39,20 +39,39 @@
   4.8MHz will be 9600 Baud
   1.2MHz will be 9600 Baud
   
+  If you get garbage output:
+  
+    1. Check baud rate as above
+    2. Check if you have anything else connected to TX/RX like an LED
+    3. Check OSCCAL (see Examples > 05.Tools > OSCCAL_Helper    
+  
   Pinout
   ------------------------------------------------------------------------------
   For ATTiny13 Arduino Pinout: https://goo.gl/ijL0of  
-
-  You don't have much flash or ram to work with.  Remember to think about data type sizes!
+  
+  Important: 
+    pinMode() must only be used with the "digital pin numbers" 0 .. n
+    pins default to INPUT, you do not need to pinMode() to INPUT if you are only
+    ever doing an analogRead() from the pin. 
+    
+    analogRead() must only be used with the "analog pin numbers" A0 .. n
+        
+  
+  Space Saving Tips
+  ------------------------------------------------------------------------------
+  
+  You don't have much flash or ram to work with.  Remember to think about 
+  datatype sizes!  Use the options under the Tools menu to reduce capabilities
+  hopefully in exchange for more code size (especially Millis and Print).
 
   Running short on memory, try this tool to help you track down optimisable areas:
-    http://sparks.gogo.co.nz/avr-ram-use.html
+    http://sparks.gogo.co.nz/avr-ram-use.html  
   
   Good Luck With Your Itsy Bitsy Teeny Weeny AVR Arduineee
   
 */
 
-const uint8_t INPUT_PIN = A2;  // Analog input pin that the potentiometer is attached to
+const uint8_t INPUT_PIN = A2;   // Analog input pin that the potentiometer is attached to
 const uint8_t analogOutPin = 1; // Analog output pin that the LED is attached to
 
 uint16_t sensorValue = 0;        // value read from the pot
@@ -63,12 +82,7 @@ void setup()
   Serial.begin(57600);         // NOTICE the baud rate specified is ignored on the T13
                               //  Instead it is hard coded as follows...
                               //  Processors at 9.6MHz ==> 57600
-                              //  Processors at 4.8 and 1.2MHz ==> 9600
-  
-  pinMode(INPUT_PIN, INPUT);   // Not strictly necessary because it will be an INPUT already at boot
-                               // but for clarity we will include it anyway.  If you are short on space
-                               // think about what the chip state is initially to see if you can
-                               // omit things like this.
+                              //  Processors at 4.8 and 1.2MHz ==> 9600 
   
   pinMode(analogOutPin, OUTPUT);
 }
