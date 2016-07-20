dbdd25cf7fccbb9d81b4c94322d2477013558f59
James Sleeman <james@gogo.co.nz>
2016-07-20 21:24:41 +1200

    Tidy up licence comments

--- avr/variants/tiny13/Tone.h
+++ avr/variants/tiny13/Tone.h
@@ -1,3 +1,15 @@
+/** Tiny13 Tone
+ * 
+ * This header contains an inline function which stands in for tone()
+ * (see pins_ardiuno.h for how exactly that happens), precalculates
+ * necessary numbers and pases them into _toneRaw() which is 
+ * implemened in Tone.cpp 
+ * 
+ * @author James Sleeman <james@gogo.co.nz>
+ * @license The MIT License (MIT) [See At End Of File]
+ *  
+ */
+
 static inline void _tone(const uint8_t pin, const uint32_t frequency, const uint32_t length)
 {
   uint16_t  prescaleDivider = 1; // The divider increases by powers of 2
@@ -116,3 +128,28 @@ static inline void _tone(const uint8_t pin, const uint32_t frequency, const uint
   
   _toneRaw(pin, ((F_CPU / prescaleDivider) / frequency), length ? length * ticksPerMs : ~(0UL), prescaleBitMask);
 }
+
+
+/*
+  The MIT License (MIT)
+
+  Copyright (c) 2016 James Sleeman
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+*/
