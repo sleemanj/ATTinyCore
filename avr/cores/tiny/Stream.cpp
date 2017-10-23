49e3461704c283dd7a832e77418ace623794f525
James Sleeman <james@gogo.co.nz>
2017-10-23 13:54:38 +1300

    Handle NO_MILLIS for Stream
    The NO_MILLIS option for Tiny13 was not working any more because Stream needed millis() and is always compiled by the build process even if it's not needed by anything.  This is not an ideal fix, but will do for now.

--- avr/cores/tiny/Stream.cpp
+++ avr/cores/tiny/Stream.cpp
@@ -30,6 +30,7 @@
 // private method to read stream with timeout
 int Stream::timedRead()
 {
+  #if !defined(NO_MILLIS)
   int c;
   _startMillis = millis();
   do {
@@ -37,11 +38,26 @@ int Stream::timedRead()
     if (c >= 0) return c;
   } while(millis() - _startMillis < _timeout);
   return -1;     // -1 indicates timeout
+  #else
+  int c; // @FIXME Why are we using an int for this?
+  // We can't use millis() to time ourselves, we will have to
+  // do this another way, timeout is no longer in milliseconds
+  // @TODO cycle-count the contents of this loop and figure out a proper
+  //   number for this, this is currently completely bogus based on
+  //   a complete and utter guess assuming 9.6MHz clock (ATTiny13 commonly)
+  uint32_t MaxLoops = _timeout << 10;
+  do {
+    c = read();
+    if (c >= 0) return c;
+  } while(MaxLoops-- > 0);
+  return -1;     // -1 indicates timeout
+  #endif
 }
 
 // private method to peek stream with timeout
 int Stream::timedPeek()
 {
+  #if !defined(NO_MILLIS)
   int c;
   _startMillis = millis();
   do {
@@ -49,6 +65,20 @@ int Stream::timedPeek()
     if (c >= 0) return c;
   } while(millis() - _startMillis < _timeout);
   return -1;     // -1 indicates timeout
+  #else
+  int c; // @FIXME Why are we using an int for this?
+  // We can't use millis() to time ourselves, we will have to
+  // do this another way, timeout is no longer in milliseconds
+  // @TODO cycle-count the contents of this loop and figure out a proper
+  //   number for this, this is currently completely bogus based on
+  //   a complete and utter guess assuming 9.6MHz clock (ATTiny13 commonly)
+  uint32_t MaxLoops = _timeout << 10;
+  do {
+    c = peek();
+    if (c >= 0) return c;
+  } while(MaxLoops-- > 0);
+  return -1;     // -1 indicates timeout
+  #endif
 }
 
 // returns peek of the next digit in the stream or -1 if timeout
